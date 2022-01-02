e72cc755ee6e4657f5a6449a29dd2673fd4cfa0b
CJ13th <ccampbell.james@gmail.com>
2022-01-02 12:14:34 +0000

    add dummy products

---
+++ product.js
@@ -0,0 +1,61 @@
+
+// Create some dummy product information - TODO: Move this to database
+
+
+const products = [
+    {
+        id: 1,
+        brand: 'Benson & Hedges',
+        name: 'Gold - 20 Cigarettes',
+        image: '/workspace/concentric/Cigarettes.jpeg',
+        description: 'Benson & Hedges has since been a mark of distinctive quality and taste.',
+        base_price: '13.99'
+    },
+    {
+        id: 2,
+        brand: "Baileys",
+        name: 'Baileys Irish Cream Liqueur',
+        image: '/workspace/concentric/Baileys.jpg',
+        description: "Whether you're baking, shaking or cocktail-making, Baileys is the perfect ingredient. Great on its own or serve over ice cream for a sophisticated twist. Want to treat someone else? Why not gift them the world's most-loved spirit.",
+        base_price: '20'
+    },
+    {
+        id: 3,
+        brand: "Farmison & Co",
+        name: '7 day dry aged half leg of lamb',
+        image: '/workspace/concentric/Lamb.jpeg',
+        description: "A British favourite, with an abundance of lean, succulent meat ideal for your Sunday or celebration roast with all the trimmings and well suited to cooking with aromatic rosemary. Skillfully prepared by our Master Butcher and left on the bone for maximum flavour, the impressive sweet taste reflects the quality of the lush pastures where our flocks mature on diets of wild flowers, herbs and natural grasses.",
+        base_price: '33.99'
+    },
+    {
+        id: 4,
+        brand: "Bradley's Fish",
+        name: 'Salmon Fillet 1kg',
+        image: '/workspace/concentric/Lamb.jpeg',
+        description: "Scottish salmon fillets. This is a farmed product from the waters off the coast of Scotland. These Skinless  salmon fillets (800g-1.1kg)  ready for sale as fresh products or to be cooked at home. The skin adds flavour to the meat which is firm and pink in colour. Although it can be eaten raw, the fillet is more commonly pan-fried or oven-roasted, before being served with a dressed salad.",
+        base_price: '21.00'
+    },
+    {
+        id: 5,
+        brand: "Kirby's Fruit",
+        name: 'Fruit Box',
+        image: '/workspace/concentric/Fruitbox.jpeg',
+        description: `
+            1kg bananas
+            4 x red apples
+            4 x green apples
+            4 x pears
+            6 x clementine’s
+            2 x kiwi fruit
+            1 x honeydew melon
+            1 x watermelon
+            1 x punnet blueberries
+            1 x punnet raspberries
+            1 x punnet strawberries
+            1 x pink grapefruit
+            1 x extra-sweet pineapple
+            500g white seedless grapes
+            500g plums`,
+        base_price: '20.00'
+    },
+]
