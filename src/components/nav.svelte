d2bc5afd2c27e1d05aae4b5dc51c01e70ddf77e2
CJ13th <ccampbell.james@gmail.com>
2022-01-02 11:35:44 +0000

    Setup DaisyUI + basic homepage

---
+++ src/components/nav.svelte
@@ -0,0 +1,16 @@
+<div class="navbar mb-2 shadow-lg bg-primary text-neutral-content rounded-box">
+    <div class="flex-none px-2 mx-2">
+      <span class="text-lg font-bold">
+              Concentric
+            </span>
+    </div> 
+    <div class="flex-1 px-2 mx-2 navbar-end" >
+      <div class="items-stretch hidden lg:flex">
+        <a class="btn btn-ghost btn-sm rounded-btn">Home</a> 
+        <a class="btn btn-ghost btn-sm rounded-btn">Explore</a> 
+        <a class="btn btn-ghost btn-sm rounded-btn">Sign up</a> 
+        <a class="btn btn-ghost btn-sm rounded-btn">About us</a>
+      </div>
+    </div> 
+  </div>
+  
